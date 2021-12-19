e5777472af6c085c92fb0e4f929ce84e0c0a32e6
Adam <adamware99@hotmail.com>
2021-12-20 02:31:56 +0400

    Updated routes to MVN format

--- backend/api/routes/User.ts
+++ backend/api/routes/User.ts
@@ -1,15 +1,33 @@
-import { Router } from "express";
-import isAuth from '@middlewear/auth-mw';
+import { Router, Request, Response } from "express";
+import { errorHandler, userHandler } from '@middlewear';
 import { userController } from '@controllers';
-const router = Router();
+const router = Router({ mergeParams: true });
 
-router.get('/me', isAuth);
+/* Assure they're authorized before going forward. */
+router.use(userHandler.isAuth);
+
+/* User Sepecfic Routes */
+router.get('/me');
 router.get('/', userController.getAllUsers);
-router.post('/');
+router.get('/:id', userController.getUserById);
 
-// router.route('/:id')
-//     .get()
-//     .delete()
-//     .put();
+
+/* User Friends Routes */
+router.route('/:id/friends')
+    .get(userController.friends.getAllFriends)
+    .post()
+    .put()
+    .delete();
+
+/* All Friend Requsts For A User */
+router.get('/:id/friends/requests', userController.friends.getAllRequests);
+
+/* Friend Request Routes */
+router.route('/:id/friends/requests/:toId')
+    .get(userController.friends.getRequestById)
+    .post(userController.friends.postRequest)
+    .put(userController.friends.updateRequest)
+    .delete(userController.friends.deleteRequest)
 
 export default router;
+
